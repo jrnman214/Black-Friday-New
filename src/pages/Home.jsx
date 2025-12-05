77a2899c4b994106fcdc7715479d4b316b7bb13f
jrnman214 <jrnman214@gmail.com>
2025-12-05 09:00:14 -0500

    feat: Add Home page with countdown, scroll animations, and tilt effects, and update styling for new UI elements.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -396,7 +396,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/credit_expunge.png" alt="Credit Expunge Mockup" className="item-mockup" />
-                                <h3>Credit Expunge Administered by ME ($2500 Value)</h3>
+                                <h3>Credit Expunge Administered by ME <span className="price-value">($2500 Value)</span></h3>
                             </div>
                             <p>Using Metro 2, Consumer Law, Factual Disputes, CFPB, I don't just use 1 method which is usually Factual disputing which is only 65% or so effective, I combine them all and even use handwritten fonts on dispute letters so that they are not automatically denied from the credit bureaus. Avoiding the Issue of having to send out STALL letters. This makes credit repair 98% effective and speeds up results by 3x.</p>
                         </div>
@@ -404,7 +404,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/mindset.svg" alt="Mindset Book Mockup" className="item-mockup" />
-                                <h3>Mindset ($97 Value)</h3>
+                                <h3>Mindset <span className="price-value">($97 Value)</span></h3>
                             </div>
                             <p>Mindset, before you start a business, in order to be successful you need to have the right mindset. It's not the same thing as just clocking into work and doing your 8 hours. Also get 30 days of mentorship in which Mindset is the first topic discussed.</p>
                         </div>
@@ -412,7 +412,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/branding.png" alt="Branding Laptop Mockup" className="item-mockup" />
-                                <h3>Branding Your Business ($197 Value)</h3>
+                                <h3>Branding Your Business <span className="price-value">($197 Value)</span></h3>
                             </div>
                             <p>How to make your brand stand out from the others. Picking the correct business name is critical. Creating your Domain name so you are easily findable leads to more sales.</p>
                         </div>
@@ -420,7 +420,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/mockup_start_business.jpg" alt="Start Business Mockup" className="item-mockup" />
-                                <h3>How To Start A Business ($47 Value)</h3>
+                                <h3>How To Start A Business <span className="price-value">($47 Value)</span></h3>
                             </div>
                             <p>How to create your LLC, Get your EIN#, Get your Bank Account so you can start building business credit.</p>
                         </div>
@@ -428,7 +428,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/mockup_personal_credit.jpg" alt="Personal Credit Mockup" className="item-mockup" />
-                                <h3>Personal Credit ($797 Value)</h3>
+                                <h3>Personal Credit <span className="price-value">($797 Value)</span></h3>
                             </div>
                             <p>Includes AI credit repair software and all the hacks to remove items from your credit report in as fast as 14 days. Utliize the CFPB, Consumer Law, FTC, Metro 2, and Factual Disputing to remove anything from your credit reported. Although in this package, we remove everything for you, This is good if you are looking to start a 6 Figure Funding Company or a Credit repair Business. Or worst case, if you get any negative item on your credit report, you will have access to my AI Software as well as all the training on how to remove anything from your report.</p>
                         </div>
@@ -436,7 +436,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/mockup_business_credit.jpg" alt="Business Credit Mockup" className="item-mockup" />
-                                <h3>Business Credit ($997 Value)</h3>
+                                <h3>Business Credit <span className="price-value">($997 Value)</span></h3>
                             </div>
                             <p>The step-by-step blueprint to build business credit in less than 90 days. Learn the secret methods to getting up to $1M in Funding using credit stacking and transfer your debt from personal to business to boost credit score</p>
                         </div>
@@ -444,7 +444,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/mockup_tradelines.jpg" alt="Tradelines Mockup" className="item-mockup" />
-                                <h3>Tradelines ($27 Value)</h3>
+                                <h3>Tradelines <span className="price-value">($27 Value)</span></h3>
                             </div>
                             <p>If you do not have a strong credit profile, Fast track to success by using others credit cards to add to your profile to boost your score by up to 160 points within 3 days. Also how you can make money by utilizing your good credit. Backdate your rent up to 2 years.</p>
                         </div>
@@ -452,7 +452,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/mockup_funding_structure.jpg" alt="Funding Structure Mockup" className="item-mockup" />
-                                <h3>Funding Structure ($297 Value)</h3>
+                                <h3>Funding Structure <span className="price-value">($297 Value)</span></h3>
                             </div>
                             <p>How to set your business up properly so you can get access to $150K in business funding with a brand-new business. This has a sequence of banks to hit first, how to set up your business account so that you will be automatically approved.</p>
                         </div>
@@ -460,7 +460,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/mockup_funding_secrets.jpg" alt="Funding Secrets Mockup" className="item-mockup" />
-                                <h3>Funding Secrets ($1997 Value)</h3>
+                                <h3>Funding Secrets <span className="price-value">($1997 Value)</span></h3>
                             </div>
                             <p>Fast track to success by knowing all the secrets to getting approved the first time. Know what credit bureaus the banks pull from, exactly how to fill out the application so the system can give you an automatic approval.</p>
                         </div>
@@ -468,7 +468,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/mockup_credit_2_cash.jpg" alt="Credit 2 Cash Mockup" className="item-mockup" />
-                                <h3>Credit 2 Cash ($497 Value)</h3>
+                                <h3>Credit 2 Cash <span className="price-value">($497 Value)</span></h3>
                             </div>
                             <p>Obtaining business credit funding is great for starting or scaling a business but it does have one drawback… You can only spend it using your business credit card. This prevents you from using it to invest in real estate as you can only put a down payment on a property using debit. But with this method, you can turn credit into cash that you can spend however you like</p>
                         </div>
@@ -476,7 +476,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/mockup_funding_business.jpg" alt="6 Figur Funding Business Mockup" className="item-mockup" />
-                                <h3>6 Figur Funding Business ($4697 Value)</h3>
+                                <h3>6 Figur Funding Business <span className="price-value">($4697 Value)</span></h3>
                             </div>
                             <p>How to generate $20,000-$50,000 a month just by getting clients business funding without doing the deals. The Funding Business in 2023 Is a hot topic, I have had mentees who made $20,000 their first month. So by Learning all the ins and outs of fixing credit, by utilizing this one skill set just 2 hours a week and you can make a 6 figur income.</p>
                         </div>
@@ -484,7 +484,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/mockup_digital_product.jpg" alt="Digital Product Mastery Mockup" className="item-mockup" />
-                                <h3>Digital Product Mastery ($497 Value)</h3>
+                                <h3>Digital Product Mastery <span className="price-value">($497 Value)</span></h3>
                             </div>
                             <p>How to generate sleep coins. So many millionaires are being made from this business model. Grant Cardone, Known for Real Estate, Makes Millions each day from selling information. Adding digital products to get leads for your business and adding upsales and downsales to pay for ads can really help you scale your business. Imagine making a product one time and you can sell it OVER AND OVER AND OVER, No need to ever restock as it's digital. No Need to ship it, just set up an email automation and go to sleep and see your banks account, hundreds of dollars richer.</p>
                         </div>
@@ -492,7 +492,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/mockup_affiliate_marketing.jpg" alt="Affiliate Marketing Mockup" className="item-mockup" />
-                                <h3>Affiliate Marketing ($197 Value)</h3>
+                                <h3>Affiliate Marketing <span className="price-value">($197 Value)</span></h3>
                             </div>
                             <p>You don't need to have a product of your own to sell to make money. You can sell other people's products and make up to 50% commissions just by sending a link and they purchase. In here, I show you how to find Affiliate Marketing opportunities, and different ways you can get paid from it without having to have your own products.</p>
                         </div>
@@ -509,7 +509,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/bonus_turbo_boost.jpg" alt="Credit History Turbo Boost Mockup" className="item-mockup" />
-                                <h3>BONUS#1 Credit History Turbo Boost ($297 Value)</h3>
+                                <h3>BONUS#1 Credit History Turbo Boost <span className="price-value">($297 Value)</span></h3>
                             </div>
                             <p>Have poor credit history/age? This will solve your problem instantly. It’s how we’re able to get 5 figures in funding for 18-year-olds who started with no credit history. No need to wait 2-3 years, Instantly Boost Scores</p>
                         </div>
@@ -517,7 +517,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/bonus_coaching.jpg" alt="1-On-1 Coaching Calls Mockup" className="item-mockup" />
-                                <h3>BONUS #2 1-On-1 Coaching Calls ($397 Value)</h3>
+                                <h3>BONUS #2 1-On-1 Coaching Calls <span className="price-value">($397 Value)</span></h3>
                             </div>
                             <p>While Going through the process of credit repair, I will give you monthly coaching calls to help you make the correct decisions with your credit. I have been featured in a few podcasts where I have shed my knowledge and experience with credit, and I will assist you as well so you can be successful. Getting good credit is cool, but maintaining is where it's at. But learn how to leverage it to the maximum.</p>
                         </div>
@@ -525,7 +525,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/bonus_funding.jpg" alt="$30K Funding Mockup" className="item-mockup" />
-                                <h3>BONUS #3 $30K Funding ($597 Value)</h3>
+                                <h3>BONUS #3 $30K Funding <span className="price-value">($597 Value)</span></h3>
                             </div>
                             <p>Receive up to $30K in Funding after completion of credit repair. Typically, scores need to be above a 680+ to receive funding. Our goal is to get you to 750+. Now while we can get clients up to $1M in Funding, that requires setting up the business, and structuring it properly. However, we give you the blueprint to successfully obtain funding up to $150K.</p>
                         </div>
@@ -533,7 +533,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/bonus_passive_income.jpg" alt="Passive Income Play Mockup" className="item-mockup" />
-                                <h3>BONUS #4 Passive Income Play ($1,197 Value)</h3>
+                                <h3>BONUS #4 Passive Income Play <span className="price-value">($1,197 Value)</span></h3>
                             </div>
                             <p>We not only want to fix your credit, but we want to teach you how to leverage it to make money passively. What we will do is figure out your individual goals, what your specialties are and give you a step-by-step personalized game plan so that you can quit that 9-5 job and start earning money month after month with little to no effort. Currently have a client named Jessica who I got her a $50K Approval and she purchased 20 vending massage chairs. She now makes $15K a month passively and does not have to do anything but collect a check.</p>
                         </div>
@@ -541,7 +541,7 @@ const Home = () => {
                         <div className="breakdown-item animate-on-scroll fade-in-up">
                             <div className="item-header">
                                 <img src="/assets/bonus_software_access.jpg" alt="Lifetime Access ALL Courses Mockup" className="item-mockup" />
-                                <h3>BONUS #5 Lifetime Access ALL Courses ($5,000 Value)</h3>
+                                <h3>BONUS #5 Lifetime Access ALL Courses <span className="price-value">($5,000 Value)</span></h3>
                             </div>
                             <p>If you want to make sleep coins, Money in your Sleep, you need to have digital products to sell. No Need to Create a Product, I have a full list of 600+ Digital eBooks +200 Video Courses that you can have and turn into your own. No need to pay someone a few hundred $$$ just to make 1 eBook. TAKE MY entire library. Instead of purchasing separately for $27-$297 Each eBook or Course, Included with this offer, You can have all of them for life to create an passive income of $10,000-$20,000 a month on autopilot</p>
                         </div>

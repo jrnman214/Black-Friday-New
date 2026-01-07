2821959398d8c0be29935a65956d9b7e208b251a
jrnman214 <jrnman214@gmail.com>
2026-01-06 20:16:47 -0500

    feat: Add New Year Sale page with dedicated video, styles, and countdown functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import NewYear from './pages/NewYear'
 import Checkout from './pages/Checkout'
 import OptinPage from './pages/OptinPage'
 
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<OptinPage />} />
                 <Route path="/xmas" element={<Home />} />
+                <Route path="/newyear" element={<NewYear />} />
                 <Route path="/checkout" element={<Checkout />} />
             </Routes>
         </Router>
